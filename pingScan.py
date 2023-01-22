1af86224c885484b6196f1c1d18a6021b1ddf497
lucidelirium <58799757+lucidelirium@users.noreply.github.com>
2023-01-21 23:38:37 -0500

    Add files via upload

---
+++ pingScan.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+network=str(sys.argv[1])
+start=int(sys.argv[2])
+stop=int(sys.argv[3])
+stop+=1
+
+for x in range(start, stop, 1):
+    lastOctet=str(x)
+    ip=(str(network+"."+lastOctet))
+    response = os.system("ping -c 1 -W 1 " + ip + "> /dev/null")
+    if response == 0:
+      print (str(ip))
